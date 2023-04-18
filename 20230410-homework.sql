7f459e16dd290795f1e0282427fee801be5bf458
hotaruZzZz <108340230+hotaruZzZz@users.noreply.github.com>
2023-04-18 16:29:12 +0800

    Update 20230410-homework.sql

--- 20230410-homework.sql
+++ 20230410-homework.sql
@@ -23,14 +23,23 @@ SELECT MAX(UnitPrice)
 FROM Products p))
 WHERE p.UnitPrice = (SELECT MIN(UnitPrice) FROM Products WHERE CategoryID = c.CategoryID);
 -- 計算出上面類別最貴和最便宜的兩個產品的價差
-
+SELECT (MAX(p.UnitPrice) - MIN(p.UnitPrice)) AS gap 
+FROM Products p, Categories c
+WHERE c.CategoryID =(
+SELECT p.CategoryID
+FROM Products p
+WHERE p.UnitPrice =
+(
+SELECT MAX(UnitPrice)
+FROM Products p
+));
 -- 找出沒有訂過任何商品的客戶所在的城市的所有客戶
-SELECT c.CustomerID, c.City
+SELECT *
 FROM Customers c
-except
-SELECT DISTINCT c.CustomerID, c.City
-FROM Orders o
-INNER JOIN Customers c ON c.CustomerID = o.CustomerID;
+WHERE NOT EXISTS(
+	SELECT * FROM Orders
+	WHERE CustomerID = c.CustomerID
+)
 -- 找出第 5 貴跟第 8 便宜的產品的產品類別
 WITH t1 AS (
 	SELECT
@@ -111,7 +120,16 @@ FROM Products
 WHERE Discontinued = 0;
 
 -- 列出購買非本國的產品的客戶
-
+SELECT DISTINCT c.CustomerID, c.ContactName, c.City
+FROM Customers c
+INNER JOIN Orders o ON o.CustomerID = c.CustomerID
+INNER JOIN [Order Details] od ON od.OrderID = o.OrderID
+WHERE od.ProductID IN (
+	SELECT  p.ProductID
+	FROM Products p 
+	INNER JOIN Suppliers s ON s.SupplierID = p.SupplierID
+	WHERE s.City <> c.City
+)
 
 -- 列出在同個城市中有公司員工可以服務的客戶
 SELECT c.CustomerID
@@ -124,7 +142,8 @@ WHERE NOT EXISTS(
 SELECT od.ProductID
 FROM [Order Details] od
 )
-----------------------------------------------------------------------------------------
+
+----------------------------------------------------------
 
 -- 列出所有在每個月月底的訂單
 SELECT o.OrderDate, o.OrderID
@@ -137,27 +156,80 @@ INNER JOIN [Order Details] od ON od.OrderID = o.OrderID
 INNER JOIN Products p ON p.ProductID = od.ProductID
 WHERE DATEPART(day,o.OrderDate) >= 20 
 -- 找出有敗過最貴的三個產品中的任何一個的前三個大客戶
-
+ 
 -- 找出有敗過銷售金額前三高個產品的前三個大客戶
 
 -- 找出有敗過銷售金額前三高個產品所屬類別的前三個大客戶
 
 -- 列出消費總金額高於所有客戶平均消費總金額的客戶的名字，以及客戶的消費總金額
-
+WITH t1 AS(
+SELECT SUM((od.UnitPrice*od.Quantity)*(1-od.Discount)) totalPrice, c.CustomerID CustomerID 
+FROM Customers c
+INNER JOIN Orders o ON o.CustomerID = c.CustomerID
+INNER JOIN [Order Details] od ON od.OrderID = o.OrderID
+GROUP BY c.CustomerID
+)
+SELECT CustomerID, totalPrice 
+FROM t1
+WHERE totalPrice > (
+SELECT AVG(totalPrice) FROM t1)
 -- 列出最熱銷的產品，以及被購買的總金額
-
+SELECT TOP 1 SUM(od.UnitPrice) sum_price, od.ProductID
+FROM [Order Details] od
+GROUP BY od.ProductID
+ORDER BY SUM(od.Quantity) DESC
 -- 列出最少人買的產品
-
+SELECT TOP 1 SUM(od.Quantity) sum_quantity, od.ProductID
+FROM [Order Details] od
+GROUP BY od.ProductID
+ORDER BY SUM(od.Quantity) 
 -- 列出最沒人要買的產品類別 (Categories)
-
+SELECT TOP 1 SUM(od.Quantity) SumQuantity, c.CategoryID
+FROM [Order Details] od
+INNER JOIN Products p ON p.ProductID = od.ProductID
+INNER JOIN Categories c ON c.CategoryID = p.CategoryID
+GROUP BY c.CategoryID
+ORDER BY SumQuantity
 -- 列出跟銷售最好的供應商買最多金額的客戶與購買金額 (含購買其它供應商的產品)
 
+
+
 -- 列出跟銷售最好的供應商買最多金額的客戶與購買金額 (不含購買其它供應商的產品)
-
--- 列出那些產品沒有人買過
-
+SELECT TOP 1 SUM((od.UnitPrice*od.Quantity)*(1-od.Discount)) totalPriec, c.CustomerID
+FROM Customers c
+INNER JOIN Orders o ON o.CustomerID = c.CustomerID
+INNER JOIN [Order Details] od ON od.OrderID = o.OrderID
+INNER JOIN Products p ON p.ProductID = od.ProductID
+WHERE p.SupplierID = (
+SELECT TOP 1 s.SupplierID
+FROM Suppliers s
+INNER JOIN Products p ON p.SupplierID = s.SupplierID
+INNER JOIN [Order Details] od ON od.ProductID = p.ProductID
+GROUP BY s.SupplierID
+ORDER BY SUM(od.Quantity) DESC)
+GROUP BY c.CustomerID
+ORDER BY totalPriec DESC
+-- 列出那些產品沒有人買過 ??
+SELECT p.ProductID
+FROM Products p
+WHERE NOT EXISTS(
+SELECT od.ProductID
+FROM [Order Details] od
+)
 -- 列出沒有傳真 (Fax) 的客戶和它的消費總金額
-
+WITH t1 AS(
+SELECT SUM((od.UnitPrice*od.Quantity)*(1-od.Discount)) totalPrice, c.CustomerID CustomerID 
+FROM Customers c
+INNER JOIN Orders o ON o.CustomerID = c.CustomerID
+INNER JOIN [Order Details] od ON od.OrderID = o.OrderID
+GROUP BY c.CustomerID
+)
+SELECT CustomerID, totalPrice
+FROM t1
+WHERE CustomerID IN(
+SELECT c.CustomerID
+FROM Customers c
+WHERE c.Fax IS NULL)
 -- 列出每一個城市消費的產品種類數量
 
 -- 列出目前沒有庫存的產品在過去總共被訂購的數量
@@ -173,6 +245,19 @@ WHERE DATEPART(day,o.OrderDate) >= 20
 -- 列出每一個客戶買最多的那一個產品與購買數量
 
 -- 按照城市分類，找出每一個城市最近一筆訂單的送貨時間
-
+SELECT ShipCity, MAX(ShippedDate) recentDate
+FROM ORDERS 
+WHERE ShipCity IS NOT NULL
+GROUP BY ShipCity;
 -- 列出購買金額第五名與第十名的客戶，以及兩個客戶的金額差距
-
+WITH t1 AS (
+	SELECT
+		ProductID, ProductName, UnitPrice,CategoryID,
+		ROW_NUMBER() OVER (
+			ORDER BY UnitPrice DESC
+		) AS NoDesc,
+		ROW_NUMBER() OVER (
+			ORDER BY UnitPrice 
+		) AS NoAsc
+	FROM Products
+);
